ad8dc0a7c2807c5e7e15131bc9c5bc29c03098b8
Bernd Busse <bernd@busse-net.de>
2015-07-28 18:45:30 +0200

    Added script for demonstration output

---
+++ demo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# ARROWLINE
+# powerline-like shell prompt generator
+#
+# file: demo.sh
+# v0.6.3 / 2015.07.16
+#
+# (c) 2015 Bernd Busse
+# The MIT License (MIT)
+#
+
+EXEC=${1}
+
+if [[ ! -x ${EXEC} ]]; then
+    echo "ERROR: cannot find ${EXEC}"
+    exit 1
+fi
+
+echo -e "$( ${EXEC} | sed -re 's/\\\[//g' -e 's/\\\]//g' -e 's/%\{//g' -e 's/%}//g' )"
+
